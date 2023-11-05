96b108636fe840b53cc6cf6c70e94f63809a5ece
Eric Kunzel <kunzel.eric@gmail.com>
2023-11-05 17:23:14 -0300

    feat: created main page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,15 @@ const config: Config = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
+      colors: {
+        'primary': "#F6F6F6",
+        'black': "#2D2D2D",
+        'gray': '#CED4DA',
+        'gradient1': "#F8F9FA",
+        'gradient2': "#CED4DA",
+      }
     },
   },
   plugins: [],
